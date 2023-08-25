b7ced60a9ba1a81f6193c49753745f81c0fa530c
waldie11 <86674066+waldie11@users.noreply.github.com>
2023-08-25 16:11:26 +0200

    sird dynamics

--- covid19_ode_inference/examples.py
+++ covid19_ode_inference/examples.py
@@ -14,12 +14,13 @@ import covid19_ode_inference as cov19_ode
 
 from covid19_ode_inference.slow_modulation import get_cpkwargs
 
-def model_cases_seropositivity(
+def model_cases_seropositivity_dead(
     N,
     cases_data,
     t_cases_data,
     seropos_data,
     t_seropos_data,
+    dead_data,
     sim_model=False,
     fact_subs=4,
     cp_reporting_kwargs=dict(num_cps=3),
@@ -70,6 +71,29 @@ def model_cases_seropositivity(
         )
         pm.Deterministic("eta_report", eta_report, dims=("t_cases_data",))
 
+        # delta_base = pm.Normal("delta_base", 0, 1) if not sim_model else 0.3
+
+        delta_base = pm.Cauchy("delta_base", -10, beta=1 ) if not sim_model else 0.025
+        t_pos_fatality, Delta_rhos_fatality, transients_fatality = cov19_ode.priors_for_cps(
+            name="fatality",
+            name_positions="t_pos_fatality",
+            name_magnitudes="Delta_rhos_fatality",
+            name_durations="transients_fatality",
+            beta_magnitude=1,
+            sigma_magnitude_fix=0.1 if sim_model else None,
+        )
+
+        ifr_t = pt.sigmoid(
+            delta_base
+            + cov19_ode.slow_modulation.sigmoidal_changepoints(
+                ts_out=t_solve_ODE,
+                positions_cp=t_pos_fatality,
+                magnitudes_cp=Delta_rhos_fatality,
+                durations_cp=transients_fatality,
+            )
+        )
+        pm.Deterministic("ifr_t", ifr_t, dims=("t_solve_ODE",))
+
         t_pos_R, Delta_rhos_R, transients_R = cov19_ode.priors_for_cps(
             name="R",
             name_positions="t_pos_R",
@@ -95,16 +119,20 @@ def model_cases_seropositivity(
         S_0 = frac_S_0 * N
         R_0 = frac_R_0 * N
         I_0 = pm.Deterministic("I_0", N - S_0 - R_0)
+        D_0 = truncater("D_0", dist=pm.Logistic.dist(0), lower=0) if not sim_model else 0.
 
         pm.Deterministic("beta_t", beta_t, dims=("t_solve_ODE",))
 
-        def SIR(t, y, args):
-            S, I, R = y
-            β, (γ, N) = args
+        def SIRD(t, y, args):
+            S, I, R, D = y
+            (β, δ), (γ, N_in) = args
+            N = S + I + R
             dS = -β(t) * I * S / N
-            dI = β(t) * I * S / N - γ * I
+            # dI = β(t) * I * S / N - γ * I - δ(t) * I
             dR = γ * I
-            return dS, dI, dR
+            dD = δ(t) * I
+            dI = -dS - dR - dD
+            return dS, dI, dR, dD
 
         integrator = cov19_ode.CompModelsIntegrator(
             ts_out=t_solve_ODE,
@@ -115,22 +143,24 @@ def model_cases_seropositivity(
             solver=diffrax.Bosh3(),  # a 3rd order method
             adjoint=diffrax.RecursiveCheckpointAdjoint(checkpoints=len(t_solve_ODE)),
         )
-        SIR_integrator = integrator.get_Op(
-            SIR, return_shapes=[() for _ in range(3)], name="SIR"
+        SIRD_integrator = integrator.get_Op(
+            SIRD, return_shapes=[() for _ in range(4)], name="SIRD"
         )
 
-        S, I, R = SIR_integrator(
-            y0=(S_0, I_0, R_0), arg_t=beta_t, constant_args=(gamma, N)
+        S, I, R, D = SIRD_integrator(
+            y0=(S_0, I_0, R_0, D_0), arg_t=(beta_t, ifr_t), constant_args=(gamma, N)
         )
 
         pm.Deterministic("S", S, dims=("t_solve_ODE",))
         pm.Deterministic("I", I, dims=("t_solve_ODE",))
         pm.Deterministic("R", R, dims=("t_solve_ODE",))
+        pm.Deterministic("D", D, dims=("t_solve_ODE",))
 
+        Δt = np.diff(t_solve_ODE)
         new_positive = cov19_ode.interpolate(
-            ts_in=t_solve_ODE[:-1] + 0.5 * np.diff(t_solve_ODE),
+            ts_in=t_solve_ODE[:-1] + 0.5 * Δt,
             ts_out=t_cases_data,
-            y=-pt.diff(S) / np.diff(t_solve_ODE),
+            y=-pt.diff(S) / Δt,
             ret_gradients=False,
             method="cubic",
         )
@@ -173,4 +203,21 @@ def model_cases_seropositivity(
 
         pm.Deterministic("Sero_t", sero_at_cases / N)
 
+        dead = cov19_ode.interpolate(
+            ts_in=t_solve_ODE,
+            ts_out=t_cases_data,
+            y=D,
+            ret_gradients=False,
+            method="cubic",
+        )
+
+        error_dead = truncater("error_dead", dist=pm.Logistic.dist(-1), lower=0)
+        pm.Normal(
+            "dead_data",
+            dead,
+            pt.sqrt(dead + 1e-5) * error_dead,
+            observed=dead_data if not sim_model else None,
+        )
+
+        pm.Deterministic("Dead_t", dead)
     return model
